fc5ce3d43631fa3b599e881e0eebaa09be1ab683
iower <iower@tutanota.com>
2023-06-23 23:24:48 +0500

    vars

--- 02-vars-fmt/02-vars-fmt.go
+++ 02-vars-fmt/02-vars-fmt.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
 	fmt.Print("Weight: ")
@@ -12,4 +15,36 @@ func main() {
 	fmt.Println()
 
 	fmt.Printf("Weight: %v, Age: %v\n", 55*0.3783, 41*365/687)
+
+	fmt.Printf("%-15v $%4v\n", "SpaceX", 94)
+	fmt.Printf("%-15v $%4v\n", "Virgin Galactic", 100)
+
+	const lightSpeed = 299792
+	var distance = 56000000
+
+	fmt.Println(distance/lightSpeed, "s")
+
+	distance = 401000000
+	fmt.Println(distance/lightSpeed, "s")
+
+	var (
+		distance2 = 56000000
+		speed2    = 100800
+	)
+	fmt.Println(distance2 / speed2)
+
+	var distance3, speed3 = 56000000, 100800
+	fmt.Println(distance3 / speed3)
+
+	var weight = 149.0
+	weight *= 0.3783
+
+	var age = 41
+	age++
+
+	var num = rand.Intn(10) + 1
+	fmt.Println(num)
+
+	num = rand.Intn(10) + 1
+	fmt.Println(num)
 }
