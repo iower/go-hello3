83685c7b9982c3da762f4693001da0b9c3aba036
iower <iower@tutanota.com>
2023-06-26 10:10:09 +0500

    conditions 2

--- 03-conditions/03-conditions.go
+++ 03-conditions/03-conditions.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -24,5 +26,44 @@ func main() {
 		fmt.Println("leap")
 	} else {
 		fmt.Println("not leap")
+	}
+
+	// switch
+	var switchCommand = 3
+	switch switchCommand {
+	case 1:
+		fmt.Println("Command 1")
+	case 2:
+		fmt.Println("Command 2")
+	default:
+		fmt.Println("Other")
+		fallthrough
+	case 1000:
+		fmt.Println("Fallthrough")
+	}
+
+	// for
+	var count = 10
+
+	for count > 0 {
+		fmt.Println(count)
+		time.Sleep(time.Second)
+		count--
+	}
+	fmt.Println("The end!")
+
+	// endless for
+	var degrees = 0
+
+	for {
+		fmt.Println(degrees)
+
+		degrees++
+		if degrees >= 360 {
+			degrees = 0
+			if rand.Intn(2) == 0 {
+				break
+			}
+		}
 	}
 }
