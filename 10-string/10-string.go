afe7576c8fdbfe39c69bd3bec3e49db70e8c53fe
iower <iower@tutanota.com>
2023-08-13 23:52:16 +0500

    string 2

--- 10-string/10-string.go
+++ 10-string/10-string.go
@@ -23,4 +23,23 @@ func main() {
 
 	fmt.Printf("%v is a %[1]T\n", "literal string")
 	fmt.Printf("%v is a %[1]T\n", `raw string literal`)
+
+	// rune, byte
+	type byte2 = uint8
+	type rune2 = int32
+
+	var pi rune = 960
+	var alpha rune = 940
+	var omega rune = 969
+	var bang byte = 33
+	fmt.Printf("%v %v %v %v\n", pi, alpha, omega, bang)
+	fmt.Printf("%c %c %c %c\n", pi, alpha, omega, bang)
+
+	grade := 'A'
+	var grade2 = 'A'
+	var grade3 rune = 'A'
+	var star byte = '*'
+	fmt.Printf("%v %v %v %v\n", grade, grade2, grade3, star)
+	fmt.Printf("%c %c %c %c\n", grade, grade2, grade3, star)
+
 }
