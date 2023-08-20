a145c5a52b2e7e2b28db1dd5a346c4d9dfccfb84
iower <iower@tutanota.com>
2023-08-20 11:42:20 +0500

    func type

--- 15-closures/15-closures.go
+++ 15-closures/15-closures.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 type kelvin float64
+type sensor func() kelvin
 
 func fakeSensor() kelvin {
 	return kelvin(rand.Intn(151) + 150)
@@ -13,6 +15,16 @@ func fakeSensor() kelvin {
 
 func realSensor() kelvin {
 	return 0
+}
+
+func measureTemperature(samples int, sensor func() kelvin) {
+	// measureTemperature(samples int, s sensor)
+	// var sensor func() kelvin
+	for i := 0; i < samples; i++ {
+		k := sensor()
+		fmt.Printf("%v° K\n", k)
+		time.Sleep(time.Second)
+	}
 }
 
 func main() {
@@ -25,4 +37,15 @@ func main() {
 
 	var sensor2 func() kelvin
 	fmt.Println(sensor, sensor2)
+
+	//
+	measureTemperature(3, fakeSensor)
 }
+
+/*
+
+func drawTable(rows int, getRow func(row int) (string, string))
+->
+type getRowFn func(row int) (string, string)
+func drawTable(rows int, getRow getRowFn)
+*/
