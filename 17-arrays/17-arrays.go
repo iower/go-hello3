14eb685270a822e058bb5f220ec96b0f4ecd34b3
iower <iower@tutanota.com>
2023-08-22 14:52:19 +0500

    Arrays

---
+++ 17-arrays/17-arrays.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var planets [8]string
+	planets[0] = "Mercury"
+	planets[1] = "Venus"
+	planets[2] = "Earth"
+
+	earth := planets[2]
+	fmt.Println(earth)
+	fmt.Println(planets)
+
+	fmt.Println(len(planets))
+	fmt.Println(planets[3])
+	fmt.Println(planets[3] == "")
+}
