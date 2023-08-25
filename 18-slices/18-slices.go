bdea15753e41fe4655d53ecb008563003715caa2
iower <iower@tutanota.com>
2023-08-25 14:24:59 +0500

    Slices 2

--- 18-slices/18-slices.go
+++ 18-slices/18-slices.go
@@ -12,4 +12,27 @@ func main() {
 	slice2 := strings[1:2]
 	fmt.Println(slice2)
 
+	// slice slice
+	slice11 := slice1[1:2]
+	fmt.Println(slice11)
+
+	// slice changes when array changes
+	strings[1] = "bb"
+	fmt.Println(slice2, slice11)
+
+	// default indexes
+	strings0 := strings[:3]
+	strings00 := strings[3:]
+	stirngs000 := strings[:]
+	fmt.Println(strings0, strings00, stirngs000)
+
+	// apply for strings
+	neptune := "Neptune"
+	tune := neptune[3:] // new string
+	fmt.Println(tune)
+	neptune = "Newstrings"
+	fmt.Println(tune)
+
+	question := "¿Cómo estás?"
+	fmt.Println(question[:6]) // "¿Cóm" - 6 bytes, not runes
 }
