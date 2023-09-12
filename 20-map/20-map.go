73262dc0ff36bb9f0abd2ebba43a3a75e29d7a54
iower <iower@tutanota.com>
2023-09-12 23:46:04 +0500

    Count freqs by maps

--- 20-map/20-map.go
+++ 20-map/20-map.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math"
 	"sort"
+	"strings"
 )
 
 func main() {
@@ -96,4 +97,24 @@ func main() {
 	}
 	sort.Float64s(unique)
 	fmt.Println(unique)
+
+	// freq
+	text := "As far as eye could reach he saw nothing but the stems of the great plants about him receding in the violet shade, and far overhead the multiple transparency of huge leaves filtering the sunshine to the solemn splendour of twilight in which he walked. Whenever he felt able he ran again; the ground continued soft and springy, covered with the same resilient weed which was the first thing his hands had touched in Malacandra. Once or twice a small red creature scuttled across his path, but otherwise there seemed to be no life stirring in the wood; nothing to fear — except the fact of wandering unprovisioned and alone in a forest of unknown vegetation thousands or millions of miles beyond the reach or knowledge of man."
+	textMap := countWords(text)
+	for key, freq := range textMap {
+		if freq > 1 {
+			fmt.Printf("%v - %v\n", freq, key)
+		}
+	}
+}
+
+func countWords(text string) map[string]int {
+	words := strings.Fields(strings.ToLower(text))
+	fmt.Println(words)
+	textMap := make(map[string]int, len(words))
+	for _, word := range words {
+		word := strings.Trim(word, ",.;—")
+		textMap[word] += 1
+	}
+	return textMap
 }
