5fe0d85fb4fa82162a5661d6d8a0acdbc7db4a3d
iower <iower@tutanota.com>
2023-09-16 23:58:45 +0500

    Structs 2

--- 22-struct/22-struct.go
+++ 22-struct/22-struct.go
@@ -30,4 +30,18 @@ func main() {
 	opportunity.lat = 2.2
 	opportunity.long = 2.22
 	fmt.Println(opportunity)
+
+	// init struct with composite literals
+	type loc struct {
+		lat, long float64
+	}
+	opportunity2 := loc{
+		lat:  1234,
+		long: 2345,
+	}
+	fmt.Println(opportunity2)
+
+	// shorted declaraion
+	spirit2 := loc{1.0, 2.0}
+	fmt.Println(spirit2)
 }
