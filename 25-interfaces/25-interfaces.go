4b73dd1a9c9878530271308006cd61146e347dcd
iower <iower@tutanota.com>
2023-09-27 23:05:34 +0500

    Interfaces 3

--- 25-interfaces/25-interfaces.go
+++ 25-interfaces/25-interfaces.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strings"
+	"time"
 )
 
 var t interface {
@@ -34,6 +35,40 @@ func shout(t talker) {
 	fmt.Println(louder)
 }
 
+type crater struct{}
+
+type starship struct {
+	laser
+}
+
+type rover struct{}
+
+func (r rover) talk() string {
+	return "whir whir"
+}
+
+type stardater interface {
+	YearDay() int
+	Hour() int
+}
+
+// func stardate(t time.Time) float64 {
+func stardate(t stardater) float64 {
+	day := float64(t.YearDay())
+	h := float64(t.Hour()) / 24.0
+	return 1000 + day + h
+}
+
+type sol int
+
+func (s sol) YearDay() int {
+	return int(s % 668)
+}
+
+func (s sol) Hour() int {
+	return 0
+}
+
 func main() {
 	fmt.Println(t)
 
@@ -45,4 +80,21 @@ func main() {
 
 	shout(martian{})
 	shout(laser(2))
+
+	// shout(crater{})
+
+	s := starship{laser(4)}
+	fmt.Println(s.talk())
+	shout(s)
+
+	r := rover{}
+	fmt.Println(r.talk())
+	shout(r)
+
+	//
+	day := time.Date(2012, 8, 6, 5, 17, 0, 0, time.UTC)
+	fmt.Printf("%.1f Curiosity has landed\n", stardate(day))
+
+	ss := sol(1422)
+	fmt.Printf("%.1f Happy birthday\n", stardate(ss))
 }
