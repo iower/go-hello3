1be977b2c7bfcbad91b8aa9f30d8530f0a2fd969
iower <iower@tutanota.com>
2023-10-02 23:59:12 +0500

    Pointers 2

--- 27-pointers/27-pointers.go
+++ 27-pointers/27-pointers.go
@@ -5,4 +5,29 @@ import "fmt"
 func main() {
 	answer := 42
 	fmt.Println(&answer)
+
+	/*
+		// errors
+		&42
+		&"indirection"
+		&true
+	*/
+
+	address := &answer
+	fmt.Println(*address)
+
+	/*
+		address++
+	*/
+	fmt.Println(*&*&answer)
+
+	// type: *int
+	fmt.Printf("address type is %T\n", address)
+
+	canada := "Canada"
+	var home *string
+	fmt.Printf("home is a %T\n", home)
+
+	home = &canada
+	fmt.Println(*home)
 }
