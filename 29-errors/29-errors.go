bcf52092cf5920ee8ce4fdbc225c3ed82dc15ad8
iower <iower@tutanota.com>
2023-11-06 23:35:27 +0500

    Errors 2

--- 29-errors/29-errors.go
+++ 29-errors/29-errors.go
@@ -6,6 +6,23 @@ import (
 	"os"
 )
 
+func out(name string) error {
+	f, err := os.Create(name)
+	if err != nil {
+		return err
+	}
+
+	_, err = fmt.Fprintln(f, "Out string 1")
+	if err != nil {
+		f.Close()
+		return err
+	}
+
+	_, err = fmt.Fprintln(f, "Out string 2")
+	f.Close()
+	return err
+}
+
 func main() {
 	files, err := ioutil.ReadDir(".")
 	if err != nil {
@@ -15,5 +32,14 @@ func main() {
 
 	for _, file := range files {
 		fmt.Println(file.Name(), file.Size(), file.ModTime(), file.Mode())
+	}
+
+	// files2, err2 := ioutil.ReadDir("/etc/hosts")
+	// fmt.Println(files2, err2)
+
+	outErr := out("out.txt")
+	if outErr != nil {
+		fmt.Println("Out err:", outErr)
+		os.Exit(1)
 	}
 }
