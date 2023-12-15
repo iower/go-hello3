af20a77ff06490a8cf1dcd95db6aed50ccdbb783
iower <iower@tutanota.com>
2023-12-15 12:57:34 +0500

    Concurrency 3

--- 32-concurrency/32-concurrency.go
+++ 32-concurrency/32-concurrency.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"log"
 	"sync"
 	"time"
 )
@@ -23,14 +25,63 @@ func (v *Visited) VisitLink(url string) int {
 }
 
 func worker() {
-	n := 0
+	pos := image.Point{X: 10, Y: 10}
+	direction := image.Point{X: 1, Y: 0}
 	next := time.After(time.Second)
 	for {
 		select {
 		case <-next:
-			n++
-			fmt.Println(n)
+			pos = pos.Add(direction)
+			fmt.Println("current pos", pos)
 			next = time.After(time.Second)
+		}
+	}
+}
+
+type command int
+
+const (
+	right = command(0)
+	left  = command(1)
+)
+
+type RoverDriver struct {
+	commandc chan command
+}
+
+func NewRoverDriver() *RoverDriver {
+	r := &RoverDriver{
+		commandc: make(chan command),
+	}
+	go r.drive()
+	return r
+}
+
+func (r *RoverDriver) drive() {
+	pos := image.Point{X: 0, Y: 0}
+	direction := image.Point{X: 1, Y: 0}
+	updateInterval := 250 * time.Millisecond
+	nextMove := time.After(updateInterval)
+	for {
+		select {
+		case c := <-r.commandc:
+			switch c {
+			case right:
+				direction = image.Point{
+					X: -direction.Y,
+					Y: direction.X,
+				}
+			case left:
+				direction = image.Point{
+					X: direction.Y,
+					Y: -direction.X,
+				}
+			}
+			log.Printf("new direction %v", direction)
+		case <-nextMove:
+			pos = pos.Add(direction)
+			log.Printf("moved to %v", pos)
+			nextMove = time.After(updateInterval)
 		}
 	}
 }
@@ -40,5 +91,5 @@ func main() {
 	defer mu.Unlock()
 
 	go worker()
-	time.Sleep(100)
+	time.Sleep(time.Hour)
 }
