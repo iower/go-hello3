cba01dc9d155654c86a384f5bd59379b3ffc8c52
iower <iower@tutanota.com>
2023-12-16 23:31:23 +0500

    Concurrency 4

--- 32-concurrency/32-concurrency.go
+++ 32-concurrency/32-concurrency.go
@@ -43,6 +43,8 @@ type command int
 const (
 	right = command(0)
 	left  = command(1)
+	start = command(2)
+	stop  = command(3)
 )
 
 type RoverDriver struct {
@@ -57,11 +59,28 @@ func NewRoverDriver() *RoverDriver {
 	return r
 }
 
+func (r *RoverDriver) Left() {
+	r.commandc <- left
+}
+
+func (r *RoverDriver) Right() {
+	r.commandc <- right
+}
+
+func (r *RoverDriver) Start() {
+	r.commandc <- start
+}
+
+func (r *RoverDriver) Stop() {
+	r.commandc <- stop
+}
+
 func (r *RoverDriver) drive() {
 	pos := image.Point{X: 0, Y: 0}
 	direction := image.Point{X: 1, Y: 0}
 	updateInterval := 250 * time.Millisecond
 	nextMove := time.After(updateInterval)
+	speed := 1
 	for {
 		select {
 		case c := <-r.commandc:
@@ -71,15 +90,22 @@ func (r *RoverDriver) drive() {
 					X: -direction.Y,
 					Y: direction.X,
 				}
+				log.Printf("new direction %v", direction)
 			case left:
 				direction = image.Point{
 					X: direction.Y,
 					Y: -direction.X,
 				}
+				log.Printf("new direction %v", direction)
+			case start:
+				speed = 1
+				log.Println("start")
+			case stop:
+				speed = 0
+				log.Println("stop")
 			}
-			log.Printf("new direction %v", direction)
 		case <-nextMove:
-			pos = pos.Add(direction)
+			pos = pos.Add(direction.Mul(speed))
 			log.Printf("moved to %v", pos)
 			nextMove = time.After(updateInterval)
 		}
@@ -91,5 +117,17 @@ func main() {
 	defer mu.Unlock()
 
 	go worker()
-	time.Sleep(time.Hour)
+
+	r := NewRoverDriver()
+	time.Sleep(time.Second)
+	r.Start()
+	time.Sleep(time.Second)
+	r.Left()
+	time.Sleep(time.Second)
+	r.Right()
+	time.Sleep(time.Second)
+	r.Stop()
+	time.Sleep(time.Second)
+	r.Right()
+	time.Sleep(time.Second)
 }
