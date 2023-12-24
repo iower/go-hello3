4adaaaf1c6f987d77a2379c0fd77d19363ecc043
iower <iower@tutanota.com>
2023-12-25 00:50:24 +0500

    web 3

--- 35-web-app/snippetbox/main.go
+++ 35-web-app/snippetbox/main.go
@@ -1,24 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hi from Snippetbox"))
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("snippet"))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "snippet with id %d", id)
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		// w.WriteHeader(405)
+		// w.Write([]byte("405 method not allowed"))
+		http.Error(w, "405 method not allowed", 405)
+		return
+	}
 	w.Write([]byte("create snippet"))
 }
 
 func test(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("test"))
+}
+
+func testJson(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"name":"Alex"}`))
 }
 
 func main() {
@@ -27,7 +51,8 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
-	mux.HandleFunc("/test", test)
+	mux.HandleFunc("/testt", test)
+	mux.HandleFunc("/json", testJson)
 
 	log.Println("Run server, localhost:4000 ")
 	err := http.ListenAndServe(":4000", mux)
