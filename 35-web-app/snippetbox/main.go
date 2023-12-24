147f3eafcb247fdea4c76ac574e414b329a82815
iower <iower@tutanota.com>
2023-12-24 23:57:45 +0500

    web 2

--- 35-web-app/snippetbox/main.go
+++ 35-web-app/snippetbox/main.go
@@ -9,9 +9,25 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hi from Snippetbox"))
 }
 
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("snippet"))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("create snippet"))
+}
+
+func test(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("test"))
+}
+
 func main() {
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/test", test)
 
 	log.Println("Run server, localhost:4000 ")
 	err := http.ListenAndServe(":4000", mux)
