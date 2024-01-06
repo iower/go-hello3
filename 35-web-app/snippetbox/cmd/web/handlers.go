b7d1223ab515958bb686b3d51d0f7b6f0701c33e
iower <iower@tutanota.com>
2024-01-07 00:51:01 +0500

    web: move handlers out

---
+++ 35-web-app/snippetbox/cmd/web/handlers.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"strconv"
+)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		app.notFound(w)
+		return
+	}
+	// w.Write([]byte("Hi from Snippetbox"))
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		app.serverError(w, err)
+	}
+}
+
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.notFound(w)
+		return
+	}
+
+	fmt.Fprintf(w, "snippet with id %d", id)
+}
+
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+
+		app.clientError(w, http.StatusMethodNotAllowed)
+		return
+	}
+	w.Write([]byte("create snippet"))
+}
+
+func test(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("test"))
+}
+
+func testJson(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"name":"Alex"}`))
+}
+
+func fileHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./ui/static/filesample.zip")
+}
--- 35-web-app/snippetbox/cmd/web/main.go
+++ 35-web-app/snippetbox/cmd/web/main.go
@@ -2,77 +2,15 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
-	"strconv"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
-}
-
-func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	// w.Write([]byte("Hi from Snippetbox"))
-	files := []string{
-		"./ui/html/home.page.tmpl",
-		"./ui/html/base.layout.tmpl",
-		"./ui/html/footer.partial.tmpl",
-	}
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
-		return
-	}
-
-	err = ts.Execute(w, nil)
-	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
-	}
-}
-
-func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-
-	fmt.Fprintf(w, "snippet with id %d", id)
-}
-
-func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		// w.WriteHeader(405)
-		// w.Write([]byte("405 method not allowed"))
-		http.Error(w, "405 method not allowed", 405)
-		return
-	}
-	w.Write([]byte("create snippet"))
-}
-
-func test(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("test"))
-}
-
-func testJson(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(`{"name":"Alex"}`))
-}
-
-func fileHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./ui/static/filesample.zip")
 }
 
 func main() {
