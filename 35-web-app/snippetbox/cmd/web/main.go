ab62fbeeb0711176b8fa0b05330b084bb4b878c6
iower <iower@tutanota.com>
2024-01-09 21:52:32 +0500

    web: open DB

--- 35-web-app/snippetbox/cmd/web/main.go
+++ 35-web-app/snippetbox/cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -18,6 +21,13 @@ func main() {
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source")
+	db, dbErr := openDB(*dsn)
+	if dbErr != nil {
+		errorLog.Fatal(dbErr)
+	}
+	defer db.Close()
 
 	app := &application{
 		errorLog,
@@ -33,4 +43,15 @@ func main() {
 	infoLog.Printf("Run server, %s\n", *addr)
 	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
