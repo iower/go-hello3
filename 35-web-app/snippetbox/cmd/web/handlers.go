beb53f1a59b8fe58533e0260c391252259ebdf61
iower <iower@tutanota.com>
2024-02-05 23:50:27 +0500

    web: template range

--- 35-web-app/snippetbox/cmd/web/handlers.go
+++ 35-web-app/snippetbox/cmd/web/handlers.go
@@ -16,6 +16,15 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	// w.Write([]byte("Hi from Snippetbox"))
+
+	s, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	data := &templateData{Snippets: s}
+
 	files := []string{
 		"./ui/html/home.page.tmpl",
 		"./ui/html/base.layout.tmpl",
@@ -27,7 +36,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = ts.Execute(w, nil)
+	err = ts.Execute(w, data)
 	if err != nil {
 		app.serverError(w, err)
 	}
